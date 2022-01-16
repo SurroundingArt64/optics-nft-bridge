ecad48bee8416b973c34da47e9de9431999c0113
SurroundingArt <97761020+SurroundingArt64@users.noreply.github.com>
2022-01-16 23:35:28 +0530

    fix: send

--- contracts/nft/ERC721Message.sol
+++ contracts/nft/ERC721Message.sol
@@ -1,9 +1,60 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity >=0.6.11;
-
-import {TypedMemView} from "@summa-tx/memview-sol/contracts/TypedMemView.sol";
+pragma experimental ABIEncoderV2;
 
 library ERC721Message {
-    using TypedMemView for bytes;
-    using TypedMemView for bytes29;
+	enum ActionType {
+		Invalid,
+		Transfer // 1
+	}
+
+	function encodeMessage(
+		address localToken,
+		uint32 senderDomain,
+		uint32 receiverDomain,
+		address remoteToken,
+		address recipient,
+		uint256 tokenId,
+		ActionType actionType
+	) internal pure returns (bytes memory) {
+		return
+			abi.encode(
+				localToken,
+				senderDomain,
+				receiverDomain,
+				remoteToken,
+				recipient,
+				tokenId,
+				actionType
+			);
+	}
+
+	// Flips the remote and local tokens
+	// Also, domain is remote domain.
+	function decodeMessage(bytes memory data)
+		internal
+		pure
+		returns (
+			address remoteToken,
+			uint32 remoteDomain,
+			uint32 localDomain,
+			address localToken,
+			address recipient,
+			uint256 tokenId,
+			ActionType actionType
+		)
+	{
+		(
+			remoteToken,
+			remoteDomain,
+			localDomain,
+			localToken,
+			recipient,
+			tokenId,
+			actionType
+		) = abi.decode(
+			data,
+			(address, uint32, uint32, address, address, uint256, ActionType)
+		);
+	}
 }
