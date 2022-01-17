cfb317d360a4f4699f0c84f827f9b063bd3aa383
SurroundingArt <97761020+SurroundingArt64@users.noreply.github.com>
2022-01-17 15:03:12 +0530

    fix: add connection manager and local domain

---
+++ deploy/0_native_nft.ts
@@ -0,0 +1,20 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { skipUnlessNative } from "../utils/network";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+	const { deployments, getNamedAccounts } = hre;
+	const { deploy } = deployments;
+
+	const { deployer } = await getNamedAccounts();
+
+	await deploy("Native721", {
+		from: deployer,
+		log: true,
+		contract: "Native721",
+		args: ["Native721", "N721"],
+		skipIfAlreadyDeployed: true,
+	});
+};
+func.tags = ["Native721"];
+func.skip = skipUnlessNative;
